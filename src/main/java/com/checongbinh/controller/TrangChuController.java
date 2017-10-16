3990bb72c82289aa4040ac853be95719eee50c62
Duc Phan <phduc.phanhongduc@gmail.com>
2017-10-16 19:20:31 +0300

    connect MySQL with JDBC

--- src/main/java/com/checongbinh/controller/TrangChuController.java
+++ src/main/java/com/checongbinh/controller/TrangChuController.java
@@ -1,5 +1,7 @@
 package com.checongbinh.controller;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,14 +14,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import org.springframework.web.servlet.ModelAndView;
 
+import com.checongbinh.entity.DatabaseThoiTrang;
+
 @Controller
 @RequestMapping("/")
 public class TrangChuController {
 	
 	@GetMapping
-	public String Default(ModelMap modelMap){
-		modelMap.addAttribute("ten", "Che Cong Binh");
-		modelMap.addAttribute("tuoi", 17);
+	public String Default(){
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("IoC.xml");
+		DatabaseThoiTrang databaseThoiTrang = (DatabaseThoiTrang) context.getBean("databasethoitrang");
+		
+		databaseThoiTrang.getListNhanVien();
 		return "trangchu";
 	}
 	
---
+++ src/main/java/com/checongbinh/entity/DatabaseThoiTrang.java
@@ -0,0 +1,46 @@
+package com.checongbinh.entity;
+
+import javax.sql.DataSource;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class DatabaseThoiTrang {
+	
+	private JdbcTemplate jdbcTemplate;
+	
+	
+	@Autowired
+	public void setDataSource(DataSource dataSource)
+	{
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+	
+	public void getListNhanVien()
+	{
+		String sql = "select * from NhanVien";
+		List<NhanVien> listNhanVien = jdbcTemplate.query(sql,new RowMapper<NhanVien>() {
+
+			public NhanVien mapRow(ResultSet rowResult, int arg1) throws SQLException {
+				// TODO Auto-generated method stub
+				
+				NhanVien nv = new NhanVien();
+				nv.tennhanvien = rowResult.getString("tenNhanVien");
+				nv.tuoi = rowResult.getInt("tuoi");
+				return nv;
+			}
+			
+		});
+		
+		for (NhanVien valueNv : listNhanVien)
+		{
+			System.out.println("Gia tri" + valueNv.tennhanvien + valueNv.tuoi);
+		}
+	}
+}
